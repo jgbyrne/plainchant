de500e2540aa4d30d3df82627cbaf8bf4a26a418
Jack Byrne <jack.byrne@durham.ac.uk>
2022-08-12 15:44:33 +0100

    submit original posts, and cleaning up message page generation

--- src/server.rs
+++ src/server.rs
@@ -13,13 +13,17 @@ use axum::{body, extract, response, routing, Extension, Router};
 use tokio;
 use tokio_util::io::ReaderStream;
 
-use bytes::Bytes;
+use bytes::{BufMut, Bytes, BytesMut};
 use mime_guess;
 
 use std::collections::HashMap;
-use std::ops::DerefMut;
+use std::net::SocketAddr;
+use std::ops::{Deref, DerefMut};
 use std::path;
 use std::sync::{Arc, Mutex};
+
+// This value is equivalent to 64 MiB in bytes;
+const FORM_MAX_LENGTH: u64 = 67_108_864;
 
 // More complex templates are handled by Pages, but these
 // simple message pages we can handle directly
@@ -40,6 +44,14 @@ fn message_page(sp: &StaticPages, message: &str) -> Html<String> {
     Html::from(sp.message_tmpl.render(&Data::values(vals)))
 }
 
+fn internal_error(sp: &StaticPages, message: &str) -> (StatusCode, Html<String>) {
+    (StatusCode::INTERNAL_SERVER_ERROR, error_page(sp, message))
+}
+
+fn bad_request(sp: &StaticPages, message: &str) -> (StatusCode, Html<String>) {
+    (StatusCode::BAD_REQUEST, message_page(sp, message))
+}
+
 async fn static_dir(
     config: Arc<Config>,
     extract::Path(path): extract::Path<path::PathBuf>,
@@ -47,24 +59,21 @@ async fn static_dir(
     let path = path.strip_prefix("/").unwrap();
     let full_path = config.static_dir.join(&path);
 
-    let file = match tokio::fs::File::open(&full_path).await {
-        Ok(f) => f,
-        Err(err) => {
-            return Err((
-                StatusCode::NOT_FOUND,
-                format!("Could not retrieve static file: {}", path.to_string_lossy()),
-            ))
+    match tokio::fs::File::open(&full_path).await {
+        Ok(file) => {
+            let mime = mime_guess::from_path(&full_path).first_or_octet_stream();
+            let headers = response::AppendHeaders([("Content-Type", mime.to_string())]);
+
+            let stream = ReaderStream::new(file);
+            let body = body::StreamBody::new(stream);
+
+            Ok((headers, body))
         },
-    };
-
-    let stream = ReaderStream::new(file);
-    let body = body::StreamBody::new(stream);
-
-    let mime = mime_guess::from_path(&full_path).first_or_octet_stream();
-
-    let headers = response::AppendHeaders([("Content-Type", mime.to_string())]);
-
-    Ok((headers, body))
+        Err(err) => Err((
+            StatusCode::NOT_FOUND,
+            format!("Could not retrieve static file: {}", path.to_string_lossy()),
+        )),
+    }
 }
 
 async fn thread<DB>(
@@ -107,9 +116,9 @@ where
                 ))
             }
         },
-        Err(err) => Ok((
-            StatusCode::INTERNAL_SERVER_ERROR,
-            error_page(sp.as_ref(), &format!("Could not acquire lock on pages")),
+        Err(err) => Ok(internal_error(
+            sp.as_ref(),
+            &format!("Could not acquire lock on pages"),
         )),
     }
 }
@@ -142,10 +151,7 @@ where
                 )
             }
         },
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            error_page(sp.as_ref(), "Could not acquire lock on pages"),
-        ),
+        Err(err) => internal_error(sp.as_ref(), "Could not acquire lock on pages"),
     }
 }
 
@@ -176,10 +182,146 @@ where
                 )
             }
         },
-        Err(err) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            error_page(sp.as_ref(), "Could not acquire lock on pages"),
-        ),
+        Err(err) => internal_error(sp.as_ref(), "Could not acquire lock on pages"),
+    }
+}
+
+type Submission = extract::ContentLengthLimit<extract::Multipart, FORM_MAX_LENGTH>;
+
+async fn create_submit<DB, FR>(
+    sp: Arc<StaticPages>,
+    pages: Arc<Mutex<pages::Pages>>,
+    actions: Arc<actions::Actions>,
+    db: Arc<DB>,
+    fr: Arc<Mutex<FR>>,
+    extract::ConnectInfo(addr): extract::ConnectInfo<SocketAddr>,
+    extract::Path(board): extract::Path<String>,
+    extract::ContentLengthLimit(mut multipart): Submission,
+) -> impl IntoResponse
+where
+    DB: 'static + db::Database + Sync + Send,
+    FR: 'static + fr::FileRack + Sync + Send,
+{
+    let board_id = {
+        match pages.lock() {
+            Ok(guard) => match guard.deref().board_url_to_id(&board) {
+                Some(board_id) => board_id,
+                None => {
+                    return Ok(response::Redirect::to("/"));
+                },
+            },
+            Err(err) => {
+                return Err(internal_error(
+                    sp.as_ref(),
+                    "Could not acquire lock on pages",
+                ));
+            },
+        }
+    };
+
+    let mut name = None;
+    let mut title = None;
+    let mut body = None;
+    let mut file_name = None;
+    let mut file = None;
+
+    while let Ok(Some(mut field)) = multipart.next_field().await {
+        match field.name() {
+            Some("name") => {
+                name = Some(
+                    field
+                        .text()
+                        .await
+                        .map_err(|err| bad_request(sp.as_ref(), "Could not parse text field"))?,
+                );
+            },
+            Some("title") => {
+                title = Some(
+                    field
+                        .text()
+                        .await
+                        .map_err(|err| bad_request(sp.as_ref(), "Could not parse title field"))?,
+                );
+            },
+            Some("body") => {
+                body = Some(
+                    field
+                        .text()
+                        .await
+                        .map_err(|err| bad_request(sp.as_ref(), "Could not parse body field"))?,
+                );
+            },
+            Some("file") => {
+                file_name = field.file_name().map(|s| s.to_string());
+
+                let mut buffer = BytesMut::with_capacity(32_768);
+                let mut space = 524_288;
+                while let Some(chunk) = field
+                    .chunk()
+                    .await
+                    .map_err(|err| bad_request(sp.as_ref(), "Could not read file"))?
+                {
+                    if space < chunk.len() {
+                        while let Ok(Some(_)) = field.chunk().await {}
+                        return Err(bad_request(sp.as_ref(), "File size limit exceeded"));
+                    }
+                    space -= chunk.len();
+                    buffer.put(chunk)
+                }
+
+                if !buffer.is_empty() {
+                    file = Some(buffer.freeze());
+                }
+            },
+            _ => {},
+        }
+    }
+
+    let file = match file {
+        Some(f) => f,
+        None => return Err(bad_request(sp.as_ref(), "You must upload a file")),
+    };
+
+    match fr.lock() {
+        Ok(mut guard) => {
+            let mut file_rack = guard.deref_mut();
+            let file_id = match actions.upload_file(file_rack, file) {
+                Ok(id) => id,
+                Err(_) => {
+                    return Err(bad_request(
+                        sp.as_ref(),
+                        "File upload failed - filetype may not be supported",
+                    ));
+                },
+            };
+
+            let submission_result = actions.submit_original(
+                db.as_ref(),
+                board_id,
+                addr.ip().to_string(),
+                body.unwrap_or_else(|| String::from("")),
+                name,
+                file_id,
+                file_name.unwrap_or_else(|| String::from("")),
+                title,
+            );
+
+            if let Err(err) = actions.enforce_post_cap(db.as_ref(), file_rack, board_id) {
+                return Err(internal_error(
+                    sp.as_ref(),
+                    "Server failure while enforcing post cap",
+                ));
+            }
+
+            match submission_result {
+                Ok(_) => Ok(response::Redirect::to(&format!("/{}/catalog", board))),
+                Err(_) => Err(internal_error(sp.as_ref(), "Failed to submit post")),
+            }
+        },
+        Err(err) => Err(internal_error(
+            sp.as_ref(),
+            "Could not obtain lock for filerack",
+        )),
     }
 }
 
@@ -216,11 +358,7 @@ where
                     .into()),
             }
         },
-        Err(err) => Err((
-            StatusCode::INTERNAL_SERVER_ERROR,
-            error_page(sp.as_ref(), "Could not acquire lock on filerack"),
-        )
-            .into()),
+        Err(err) => Err(internal_error(sp.as_ref(), "Could not acquire lock on filerack").into()),
     }
 }
 
@@ -245,11 +383,7 @@ where
                     .into()),
             }
         },
-        Err(err) => Err((
-            StatusCode::INTERNAL_SERVER_ERROR,
-            error_page(sp.as_ref(), "Could not acquire lock on filerack"),
-        )
-            .into()),
+        Err(err) => Err(internal_error(sp.as_ref(), "Could not acquire lock on filerack").into()),
     }
 }
 
@@ -331,6 +465,19 @@ pub async fn serve<DB, FR>(
                 move |path| thumbnails(sp, fr, path)
             }),
         )
+        .route(
+            "/:board/submit",
+            routing::post({
+                let (sp, pages, actions, db, fr) = (
+                    sp.clone(),
+                    pages.clone(),
+                    actions.clone(),
+                    db.clone(),
+                    fr.clone(),
+                );
+                move |conn, path, form| create_submit(sp, pages, actions, db, fr, conn, path, form)
+            }),
+        )
         .fallback(
             {
                 let sp = sp.clone();
@@ -340,7 +487,7 @@ pub async fn serve<DB, FR>(
         );
 
     axum::Server::bind(&config.addr)
-        .serve(router.into_make_service())
+        .serve(router.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .expect("Server quit unexpectedly");
 }
