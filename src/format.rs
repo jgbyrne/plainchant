b83120da8132b530f7a873e6cf642857a587a4c7
Jack Byrne <jgbyrne9@gmail.com>
2024-07-19 19:29:31 +0200

    Make URLs clickable

--- src/format.rs
+++ src/format.rs
@@ -2,8 +2,9 @@ use crate::site::Feather;
 use crate::util;
 use chrono::{TimeZone, Utc};
 use lazy_static::lazy_static;
-use regex::Regex;
+use regex::{Captures, Regex};
 use std::collections::HashSet;
+use url::Url;
 
 pub fn utc_timestamp(ts: u64) -> String {
     Utc.timestamp(ts.try_into().unwrap_or(0), 0).to_string()
@@ -49,12 +50,19 @@ pub fn humanise_time(ts: u64) -> String {
     String::from("less than a minute ago")
 }
 
+enum InlineFeature {
+    Reply,
+    Url,
+}
+
 pub fn annotate_post(body: &str, posts: &HashSet<u64>) -> String {
     lazy_static! {
         // Match quoted (greentext) lines
         static ref QUOTED: Regex = Regex::new(r"^\s*>(?:$|[^>])").unwrap();
         // Capture replies of form >>390290 and >>>/blah/2939404
         static ref REPLY: Regex = Regex::new(r"(>>)(?:([\d]+)|>/(\w+)/([\d]+))(?:$|\W)").unwrap();
+        // Capture possible URLs
+        static ref URL: Regex = Regex::new(r"(http|https)://[A-Za-z0-9\-_~:/?.#@!$&'()*+,;%=]+").unwrap();
     }
 
     let mut out = String::new();
@@ -66,54 +74,86 @@ pub fn annotate_post(body: &str, posts: &HashSet<u64>) -> String {
         }
         let mut left = 0;
 
-        for reply in REPLY.captures_iter(line) {
-            let start = reply.get(1).unwrap().start();
+        let replies = REPLY.captures_iter(line).map(|m| (InlineFeature::Reply, m));
+        let urls = URL.captures_iter(line).map(|m| (InlineFeature::Url, m));
 
+        let mut matches = replies
+            .chain(urls)
+            .collect::<Vec<(InlineFeature, Captures)>>();
+
+        matches.sort_by_key(|(_, m)| m.get(1).unwrap().start());
+
+        for (f, m) in matches {
+            let start = m.get(1).unwrap().start();
             out.push_str(&line[left..start]);
 
-            let local = match reply.get(3) {
-                Some(_) => false,
-                None => true,
-            };
-            if local {
-                let post_num = reply.get(2).unwrap();
-                let right = post_num.end();
+            let right = match f {
+                InlineFeature::Reply => {
+                    let reply = m;
 
-                if let Ok(num) = &post_num.as_str().parse::<u64>() {
-                    // If the post is in the same thread, we link to the
-                    // anchor so the browser need not make a request.
-                    // Otherwise we link directly to the post.
-                    let link = match posts.contains(&num) {
-                        true => format!("#{}", num),
-                        false => format!("./{}", num),
+                    let local = match reply.get(3) {
+                        Some(_) => false,
+                        None => true,
                     };
-                    out.push_str(&format!("<a href='{}'>&gt;&gt;{}</a>", &link, num));
-                } else {
-                    out.push_str(&line[start..right]);
-                }
 
-                left = right;
-            } else {
-                let board_id = reply.get(3).unwrap();
-                let post_num = reply.get(4).unwrap();
-                let right = post_num.end();
+                    let right;
 
-                let url = board_id.as_str();
-                let parsed_post_num = post_num.as_str().parse::<u64>();
-                let board_plausible = url.len() < 7 && url.chars().all(|c| c.is_alphanumeric());
+                    if local {
+                        let post_num = reply.get(2).unwrap();
+                        right = post_num.end();
 
-                if let (Ok(num), true) = (&parsed_post_num, board_plausible) {
-                    // TODO: This link might be flaky (should it really be absolute path?)
-                    out.push_str(&format!(
-                        "<a href='/{0}/thread/{1}'>&gt;&gt;&gt;/{0}/{1}</a>",
-                        &url, &num
-                    ));
-                } else {
-                    out.push_str(&line[start..right]);
-                }
+                        if let Ok(num) = &post_num.as_str().parse::<u64>() {
+                            // If the post is in the same thread, we link to the
+                            // anchor so the browser need not make a request.
+                            // Otherwise we link directly to the post.
+                            let link = match posts.contains(&num) {
+                                true => format!("#{}", num),
+                                false => format!("./{}", num),
+                            };
+                            out.push_str(&format!("<a href='{}'>&gt;&gt;{}</a>", &link, num));
+                        } else {
+                            out.push_str(&line[start..right]);
+                        }
+                    } else {
+                        let board_id = reply.get(3).unwrap();
+                        let post_num = reply.get(4).unwrap();
+                        right = post_num.end();
 
-                left = right;
-            }
+                        let url = board_id.as_str();
+                        let parsed_post_num = post_num.as_str().parse::<u64>();
+                        let board_plausible =
+                            url.len() < 7 && url.chars().all(|c| c.is_alphanumeric());
+
+                        if let (Ok(num), true) = (&parsed_post_num, board_plausible) {
+                            // TODO: This link might be flaky (should it really be absolute path?)
+                            out.push_str(&format!(
+                                "<a href='/{0}/thread/{1}'>&gt;&gt;&gt;/{0}/{1}</a>",
+                                &url, &num
+                            ));
+                        } else {
+                            out.push_str(&line[start..right]);
+                        }
+                    }
+
+                    right
+                },
+                InlineFeature::Url => {
+                    let url = m;
+                    let full_url = url.get(0).unwrap();
+                    let right = full_url.end();
+
+                    match Url::parse(full_url.as_str()) {
+                        Ok(_) => {
+                            out.push_str(&format!("<a href='{0}'/>{0}</a>", full_url.as_str()))
+                        },
+                        Err(_) => out.push_str(&line[start..right]),
+                    }
+
+                    right
+                },
+            };
+
+            left = right;
         }
 
         out.push_str(&line[left..]);
