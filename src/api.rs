799d89627f514a8668d84766adc5ef2891cddaee
Jack Byrne <jgbyrne9@gmail.com>
2026-03-17 02:06:01 +0100

    flesh out api types

--- src/api.rs
+++ src/api.rs
@@ -1,6 +1,7 @@
 use crate::db;
 use crate::fr;
 use crate::state::{DbState, PlainchantState};
+use crate::util::{ErrOrigin, PlainchantErr};
 
 use axum::Json;
 use axum::extract::State;
@@ -10,26 +11,59 @@ use axum::{Router, routing};
 use serde::Serialize;
 
 #[derive(Serialize)]
+struct ApiError {
+    message: String,
+}
+
+type ApiResponse<T> = (StatusCode, Json<T>);
+type ApiErrorResponse = (StatusCode, Json<ApiError>);
+
+type ApiResult<T> = Result<ApiResponse<T>, ApiErrorResponse>;
+
+fn api_ok<T>(inner: T) -> ApiResult<T> {
+    Ok((StatusCode::OK, Json(inner)))
+}
+
+impl From<PlainchantErr> for ApiErrorResponse {
+    fn from(err: PlainchantErr) -> Self {
+        let code = match err.origin {
+            ErrOrigin::Web(c) => {
+                StatusCode::from_u16(c).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR)
+            },
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
+        (code, Json(ApiError { message: err.msg }))
+    }
+}
+
+#[derive(Serialize)]
 struct ApiBoard {
-    pub url:   String,
-    pub title: String,
+    pub url:           String,
+    pub title:         String,
+    pub post_cap:      u16,
+    pub archive_cap:   u16,
+    pub bump_limit:    u16,
+    pub next_post_num: u64,
 }
 
 async fn boards<DB: db::Database>(
     State(DbState { db }): State<DbState<DB>>,
-) -> (StatusCode, Json<Vec<ApiBoard>>) {
-    let api_boards = match db.get_boards() {
-        Ok(boards) => boards
-            .into_iter()
-            .map(|board| ApiBoard {
-                url:   board.url,
-                title: board.title,
-            })
-            .collect::<Vec<ApiBoard>>(),
-        Err(_) => unimplemented!(),
-    };
+) -> ApiResult<Vec<ApiBoard>> {
+    let api_boards = db
+        .get_boards()?
+        .into_iter()
+        .map(|board| ApiBoard {
+            url:           board.url,
+            title:         board.title,
+            post_cap:      board.post_cap,
+            archive_cap:   board.archive_cap,
+            bump_limit:    board.bump_limit,
+            next_post_num: board.next_post_num,
+        })
+        .collect::<Vec<ApiBoard>>();
 
-    (StatusCode::OK, Json(api_boards))
+    api_ok(api_boards)
 }
 
 pub fn get_api_router<DB, FR>() -> Router<PlainchantState<DB, FR>>
